3c81fbe30a68ce71a8d5feb2c3c33964e8bd064d
Daisuke Inoue <hibariyashiki@gmail.com>
2017-07-01 16:15:36 +0900

    n-next

--- Kenrituhitiyutukuwanmahe.el
+++ Kenrituhitiyutukuwanmahe.el
@@ -11,6 +11,10 @@
     "returns the prev item the item you order in the list."
     (-first-item (kh-next-phalanx index list)))
 
+(defun kh-n-next-item (index n list)
+    "returns the n-next item of the item you order."
+    (-first-item (kh-n-next-phalanx index n list)))
+
 (defun kh-phalanx (index list)
     "returns the list whose first item is what you order."
     (-rotate (- index) list))
@@ -22,6 +26,10 @@
 (defun kh-next-phalanx (index list)
     "returns the list whose first item is next of what you order."
     (-rotate (+ (- index) -1) list))
+
+(defun kh-n-next-phalanx (index list)
+    "returns the list whose first item is next of what you order."
+    (-rotate (+ (- index) (- n)) list))
 
 (defun kh-current-file-index (ext)
     (let*
@@ -46,20 +54,23 @@
             (next-file (kh-next-item index list)))
         next-file))
 
+(defun kh-n-next-file (ext n)
+    (let*
+        (index (kh-current-file-index ext))
+        (list (f--files "." (equal (f-ext it) ext)))
+        (n-next-file (kh-n-next-item index n list))))
+
+
 (defun kh-find-prev-file (ext)
     (interactive)
-    (find-file (kh-prev-file ext)))
+    (kh-find-n-next-file -1 ext))
 
 (defun kh-find-next-file (ext)
     (interactive)
-    (find-file (kh-next-file ext)))
+    (kh-find-n-next-file 1 ext))
 
-(defun kh-find-prev-file-ksu ()
+(defun kh-find-n-next-file (n ext)
     (interactive)
-    (kh-find-prev-file "shell"))
-
-(defun kh-find-next-file-ksu ()
-    (interactive)
-    (kh-find-next-file "shell"))
+    (find-file (kh-n-next-file n ext)))
 
 (provide 'Kenrituhitiyutukuwanmahe)
